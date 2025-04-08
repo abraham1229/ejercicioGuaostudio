7234510cbbb9fb5c0d4d66ff5d9cfa841c965478
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-08 14:32:30 -0600

    Se agrega primer test de prueba

--- backend/src/__test__/prueba.test.ts
+++ backend/src/__test__/prueba.test.ts
@@ -10,14 +10,16 @@ describe('Registro de usuario', () => {
 
   it('Debería crear un nuevo usuario y responder con 201 y mensaje de éxito', async () => {
     const newUser = {
-      username: "TestUser",
-      email: "testuser@example.com",
+      username: "Rabi Soto",
+      email: "testuser1@example.com",
       password: "password123"
     };
 
     const response = await request(app)
       .post('/api/users/register')
       .send(newUser);
+    
+    expect(response.status).toEqual(201) 
     expect(response.text).toEqual("Registro creado correctamente");
   });
 });
