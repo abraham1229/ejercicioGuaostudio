9b7a477b4039b5084f002d82407592635921a3ed
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-09 16:44:44 -0600

    Formato config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,21 +1,21 @@
-import mongoose from 'mongoose'
-import colors from 'colors'
+import mongoose from "mongoose";
+import colors from "colors";
 //Conection to database
 export const connectDB = async () => {
-  try { 
-    const {connection} = await mongoose.connect(process.env.MONGO_URI)
-    const url = `${connection.host}:${connection.port}`
-    console.log(colors.cyan.bold(`MongoDB conectado en ${url}`))
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+    const url = `${connection.host}:${connection.port}`;
+    console.log(colors.cyan.bold(`MongoDB conectado en ${url}`));
   } catch (error) {
-    console.log(colors.red(error.message))
-    process.exit(1)
+    console.log(colors.red(error.message));
+    process.exit(1);
   }
-}
+};
 
 export const disconnectDB = async (): Promise<void> => {
   try {
     await mongoose.connection.close();
-    console.log('MongoDB desconectado');
+    console.log("MongoDB desconectado");
   } catch (error) {
     console.error(`Error al desconectar MongoDB: ${error}`);
   }
