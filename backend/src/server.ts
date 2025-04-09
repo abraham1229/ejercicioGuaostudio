fe459fbe4d0d07b12c63da972aae742f9c102198
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-09 16:41:57 -0600

    Formato a server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,22 @@
-import express from 'express' //ESM (es la nueva version que sustituye require)
-import cors from 'cors'
-import 'dotenv/config'
-import router from './router'
-import { connectDB } from './config/db'
-import { corsConfig } from './config/cors'
+import express from "express"; //ESM (es la nueva version que sustituye require)
+import cors from "cors";
+import "dotenv/config";
+import router from "./router";
+import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
 
-if (process.env.NODE_ENV !== 'test') {
-  connectDB()
+if (process.env.NODE_ENV !== "test") {
+  connectDB();
 }
 
 const app = express(); // Instancia del servidor
 
 //Cors
-app.use(cors(corsConfig))
+app.use(cors(corsConfig));
 
 //Leer datos de formularios (es middleware global)
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api", router);
 
-app.use('/api',router)
-
-
-export default app
+export default app;
