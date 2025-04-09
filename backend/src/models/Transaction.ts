5f4bed65f6bbc19a655064524db5004392136919
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-09 16:50:22 -0600

    Format models

--- backend/src/models/Transaction.ts
+++ backend/src/models/Transaction.ts
@@ -1,16 +1,20 @@
-import mongoose, { Schema, Document, Types } from 'mongoose';
+import mongoose, { Document, Schema, Types } from "mongoose";
 
-export type TransactionStatus = 'pending' | 'authorized' | 'completed' | 'failed';
+export type TransactionStatus =
+  | "pending"
+  | "authorized"
+  | "completed"
+  | "failed";
 
 export interface ITransaction extends Document {
-  sender: Types.ObjectId;     
-  recipient: Types.ObjectId; 
-  amount: number;            
-  currency: string;          
-  status: TransactionStatus; 
-  metadata?: string;         
-  createdAt: Date;           
-  updatedAt: Date;           
+  sender: Types.ObjectId;
+  recipient: Types.ObjectId;
+  amount: number;
+  currency: string;
+  status: TransactionStatus;
+  metadata?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 // Definición del Schema de Transacción
@@ -18,12 +22,12 @@ const transactionSchema = new Schema(
   {
     sender: {
       type: Schema.Types.ObjectId,
-      ref: 'User',  
+      ref: "User",
       required: true,
     },
     recipient: {
       type: Schema.Types.ObjectId,
-      ref: 'User',      
+      ref: "User",
       required: true,
     },
     amount: {
@@ -33,23 +37,26 @@ const transactionSchema = new Schema(
     currency: {
       type: String,
       required: true,
-      default: 'MXN',
+      default: "MXN",
     },
     status: {
       type: String,
-      enum: ['pending', 'authorized', 'completed', 'failed'],
-      default: 'pending',
+      enum: ["pending", "authorized", "completed", "failed"],
+      default: "pending",
       required: true,
     },
     metadata: {
       type: String,
-      default: '',
+      default: "",
     },
   },
   { timestamps: true }
 );
 
 // Se crea el modelo 'Transaction' usando el schema definido y se exporta.
-const Transaction = mongoose.model<ITransaction>('Transaction', transactionSchema);
+const Transaction = mongoose.model<ITransaction>(
+  "Transaction",
+  transactionSchema
+);
 
 export default Transaction;
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,43 +1,44 @@
-import mongoose, {Schema, Document} from 'mongoose'
+import mongoose, { Document, Schema } from "mongoose";
 
-//Informacion de typescript 
+//Informacion de typescript
 export interface IUser extends Document {
-  username: string
-  email: string,
-  password: string,
-  balance: number,
-  createdAt: Date,
-  updatedAt: Date
+  username: string;
+  email: string;
+  password: string;
+  balance: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 //Codigo de Schema en mongo
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    balance: {
+      type: Number,
+      default: 100,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  balance: {
-    type: Number,
-    default: 100
-  },
-},
-{ timestamps: true }
-)
+  { timestamps: true }
+);
 
 //<User> es un generic
-const User = mongoose.model<IUser>('User', userSchema)
+const User = mongoose.model<IUser>("User", userSchema);
 
-export default User
+export default User;
