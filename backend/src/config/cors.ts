d11ed63583c3fd35d020463077936453af6bb6f8
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-09 16:43:38 -0600

    Formato de cors

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,21 +1,7 @@
-import {CorsOptions} from 'cors'
+import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-
-    // const whiteList = [process.env.FRONTEND_URL]
-    // if (process.argv[2] === '--api') {
-    //   whiteList.push(undefined)
-    // }
-    // if (process.env.NODE_ENV === 'test') {
-    //   whiteList.push(undefined);
-    // }
-    
-    // if (whiteList.includes(origin)) {
-    //   callback(null, true) //Se acepta conexion
-    // } else {
-    //   callback(new Error('Error de CORS'))
-    // }
-    callback(null, true)
-  }
-}
+    callback(null, true);
+  },
+};
