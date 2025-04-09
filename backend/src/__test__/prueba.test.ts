809f7d67a30ec51c4f03d16e518a6aec2603bd9a
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-08 22:18:49 -0600

    Se agrega prueba de create y login

--- backend/src/__test__/prueba.test.ts
+++ backend/src/__test__/prueba.test.ts
@@ -10,19 +10,36 @@ afterAll(async () => {
   await disconnectInMemoryDB();
 });
 
-describe('Registro de Usuario', () => {
-  it('Debería crear un nuevo usuario y responder con 201 y mensaje de éxito', async () => {
-    const newUser = {
-      username: 'testuser',
-      email: 'test@example.com',
-      password: 'password123'
-    };
+const userTestCreate = {
+  username: 'testuser',
+  email: 'test@example.com',
+  password: 'password123'
+}; 
 
-    const response = await request(app)
-      .post('/api/users/register')
-      .send(newUser);
-    
-    expect(response.status).toBe(201);
-    expect(response.text).toEqual('Registro creado correctamente');
+const userTestLogin = {
+  email: 'test@example.com',
+  password: 'password123'
+};
+
+
+describe('Test de usuarios', () => { 
+  describe('Registro de Usuario', () => {
+    it('Debería crear un nuevo usuario y responder con 201 y mensaje de éxito', async () => {
+      const response = await request(app)
+        .post('/api/users/register')
+        .send(userTestCreate);
+      
+      expect(response.status).toBe(201);
+      expect(response.text).toEqual('Registro creado correctamente');
+    });
   });
-});
+  describe('Login de Usuario', () => {
+    it('Debería aceptar credenciales creadas y responder con 200 y JWT', async () => {
+      const response = await request(app)
+        .post('/api/users/login')
+        .send(userTestLogin);
+      
+      expect(response.status).toBe(200);
+    });
+  });
+})
